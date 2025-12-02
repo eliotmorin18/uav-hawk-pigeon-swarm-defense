48504ba58d947ecca05a94d7f048c02cf9d8e0ec
Hugo TREBERT <hugo.trebert@telecom-sudparis.eu>
2025-12-02 11:47:14 +0100

    plotting trajectory

--- src/main.py
+++ src/main.py
@@ -3,26 +3,33 @@ import matplotlib.pyplot as plt
 
 from core.dynamics import rk4_step
 from core.converter import control_converter
+from models.uav import UAV
 
 def main():
-    # Initial state: [x, y, z, V, mu, phi]
-    state = np.array([0., 0., 0., 20., 0.0, 0.0])
-    dt = 0.01
+    dt = 0.1
     N = 1000
-    trajectory = np.zeros((N, len(state)))
 
-    # Example: simple climb with bank
-    controls = {'Vdot': 0.0, 'mudot': 0.02, 'phidot': 0.0, 'gamma': np.deg2rad(20)}
+    # Initial state: [x, y, z, V, mu, phi]
+    uav = UAV()
+
+    uav.controls = {
+            "Vdot": 0.0,
+            "mudot": 0.02,  # Slight climb
+            "phidot": 0.00,
+            "gamma": 0.1    # Small flight path angle
+        }
+
+    trajectory = np.zeros((N, 6))
+
+    for i in range(N):
+        trajectory[i, :] = uav.state
+        uav.step(dt)
 
     # (Optional) Example using control_converter
     # u = np.array([2.0, 0.5, 0.1])
     # mu, phi = state[4], state[5]
     # nx, nf, gamma = control_converter(u, mu, phi)
     # controls = {'nx': nx, 'nf': nf, 'gamma': gamma}
-
-    for i in range(N):
-        trajectory[i] = state
-        state = rk4_step(state, controls, dt)
 
     # Plot trajectory
     fig = plt.figure(figsize=(8, 6))
@@ -35,7 +42,7 @@ def main():
     ax.legend()
     plt.show()
 
-    print("final state:", state)
+    print("final state:", uav.state)
 
 
 if __name__ == "__main__":
